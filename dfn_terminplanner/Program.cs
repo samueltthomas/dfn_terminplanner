e02c0b5cdce7cc6a8da081b8b42434328f2b0e77
Samuel <samuelthomas70@gmail.com>
2018-05-13 19:35:16 +0200

    Add project files.

---
+++ dfn_terminplanner/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dfn_terminplanner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
